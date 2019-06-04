33ec469080ca5e7ee14750dc694919506674898c
abhishek17 <abhishek.chugh21@gmail.com>
2019-06-04 15:28:06 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,request, render_template
+app = Flask(__name__)
+
+@app.route("/hi")
+def index():
+    
+    name = request.args.get('name')
+    age = request.args.get('age')
+    return render_template("index.html",name = name,age= age)
+
+@app.route("/login", methods = ['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == password:
+            return "Successfully Logged iN "
+        else:
+            return "Incorect username/password"
+if __name__ == "__main__":
+    app.run(use_reloader = True,debug = True)
